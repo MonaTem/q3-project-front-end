c4b3e490c05b884184956d45adc615d2e595f906
Mona Temchin <monatemchin@gmail.com>
2018-03-04 10:16:14 -0500

    Reinstalled reactstrap, changed Cards.js, App.js, axios-cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       // <BrowserRouter basename="/my-app">
-      <BrowserRouter>
+      <Router>
         <div className="App">
-          <Home />
+          <Cards />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect, Link } from 'react-router-dom';
 
 class Home extends Component {
     state = {
@@ -9,33 +9,14 @@ class Home extends Component {
     render () {
         return (
             <div className="Cards">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><NavLink
-                                to="/cards/"
-                                exact
-                                activeClassName="all-cards"
-                                activeStyle={{
-                                    color: '#fa923f',
-                                    textDecoration: 'underline'
-                                }}>Cards</NavLink></li>
-                            <li><NavLink to={{
-                                pathname: '/card'
 
-                            }}>Card</NavLink></li>
-                        </ul>
-                    </nav>
-                </header>
-                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Switch>
-                    <Route path="/cards" component={Cards} /> : null}
-                    <Route path="/card" component={Card} />
-                  <Route render={() => <h1>Not found/h1>}/>
-                    {/* <Redirect from="/" to="/posts" /> */}
-                    {/* <Route path="/" component={Posts} /> */}
-                </Switch>
+         <h1>Let us tell you about our coffees.</h1>
+<ul>
+  <li><Link to='/coffee/peruvian'>Peruvian</Link></li>
+  <li><Link to='/coffee/kona'>Kona</Link></li>
+</ul>
+
+
             </div>
         );
     }
