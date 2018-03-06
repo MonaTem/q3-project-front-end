db291ad2e8832872706cc6dc51ac7ef2eeb335b2
Mona Temchin <monatemchin@gmail.com>
2018-03-06 00:50:13 -0500

    Fixed height for card thumbnails on home page

--- src/components/Cardline.js
+++ src/components/Cardline.js
@@ -5,7 +5,7 @@ import './Cardline.css';
 const Cardline = props => (
     <div className="Cardline card-deck" onClick={props.clicked}>
       <div className="card">
-            <img className="card-img-top img-fluid" src={props.card_image_url} height="50" width="50" alt="Tarot card" />
+            <img className="card-img-top img-fluid" src={props.card_image_url} height="100" width="50" alt="Tarot card" />
             <div className="card-footer">
                <p className="card-text">{props.cardname}</p>
             </div>
