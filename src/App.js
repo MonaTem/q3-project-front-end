6a24bd81967ce4bebb162d29348b766614a69927
Mona Temchin <monatemchin@gmail.com>
2018-03-08 13:12:25 -0500

    Refactored app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-// eslint-disable-next-line
-import { Route, Switch, Redirect } from 'react-router-dom'
 import './App.css';
 import Cards from './components/Cards';
-// eslint-disable-next-line
 import Card from './components/Card';
+import Data from './data/Data';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        clickedCardId: null
-    };
-  }  
-  render () {
+      clickedCardId: null
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleReturnClick = this.handleReturnClick.bind(this);
+  }
+
+  handleClick(id) {
+    this.setState({clickedCardId: id});
+  }
+
+  handleReturnClick() {
+    this.setState({clickedCardId: null});
+  }
+
+  render() {
+    let selectedCards = Data.filter(Card => Card.id === this.state.clickedCardId)
     return (
-      <div>
+      <React.Fragment>
         {this.state.clickedCardId === null ?
-          <Cards /> : <Card id={this.state.clickedCardId} />}
-      </div>
+          <Cards handleClick={this.handleClick} data={Data} /> :
+          <Card
+            Card={selectedCards[0]} handleReturnClick={this.handleReturnClick}
+          />
+        }
+        {/* <Card id={3} />
+        <Cards /> */}
+      </React.Fragment>
     );
   }
 }
