ce95a0bb16b35e7820a3245877463bb5eab5249b
Mona Temchin <monatemchin@gmail.com>
2018-03-08 13:55:53 -0500

    Refactored Cards to functional component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,82 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 // eslint-disable-next-line
 // eslint-disable-next-line
 import Card from './Card';
-import axios from '../axios-cards';
 import Cardline from './Cardline';
 import './Cardline.css';
 
-class Cards extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        loading: true,
-        card: [],
-        clicked: false
-    };
-    this.cardSelectedHandler = this.cardSelectedHandler.bind(this);
-  }
-
-
-
-  componentDidMount() {
-
-      axios.get('/cards')
-          .then(res => {
-           const fetchedCards = [];
-              for (let key in res.data) {
-                  fetchedCards.push({
-                      ...res.data[key],
-                      id: key
-                  });
-              //console.log("this is what we fetched " + fetchedCards);
-              }
-
-              this.setState({loading: false, card: fetchedCards, clicked: false});
-          })
-          .catch(err => {
-              this.setState({loading: false});
-          });
-   }
-
-
-   // cardSelectedHandler = ( id ) => {
-   //
-   //   this.setState({clicked: true});
-   //
-   //   <Card id={ id } clicked={true} />
-   //
-   // }
-
-    render () {
-        let cards = <h1>Esoteric Interpretations of Tarot Cards</h1>
-
-         console.log(this.state.clicked);
-
-         if (this.state.clicked === false) {
-
-         cards =  this.state.card.map(card => {
-                    return (
-                      <div key={card.id} >
-                        <Cardline
-                           cardname={card.cardname}
-                           card_image_url={card.card_image_url}
-                           // onClick={() => this.cardSelectedHandler( this.card.id )}
-                           cardinfo={card.cardinfo}
-                         />
-                       </div>
-                     );
-          })
-
-       return (
-              <div className="card-deck" >
-                <h1>Esoteric Interpretations of Tarot Cards</h1>
-              {cards}
-              </div>
-       );
-     } else
-       return <Card/>
-    }
+const Cards = ({handleClick, data}) => {
+  return (
+    <div className="card-deck" >
+      <h1>Esoteric Interpretations of Tarot Cards</h1>
+      {data.map( card =>
+        <div className="card" key={card.id} onClick={() => handleClick(card.id)}>
+          <Cardline
+             cardname={card.cardname}
+             card_image_url={card.card_image_url}
+             cardinfo={card.cardinfo}
+           />
+       </div>
+      )}
+    </div>
+  );
 }
 
-export default Cards;
+export default Cards
