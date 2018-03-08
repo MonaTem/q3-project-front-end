862708c0ba237c48b5ef25a5c0252d03fdc8947e
Mona Temchin <monatemchin@gmail.com>
2018-03-08 16:42:35 -0500

    Fixed compile errors in App.js

--- src/App.js
+++ src/App.js
@@ -2,32 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 import Cards from './components/Cards';
 import Card from './components/Card';
-import Data from './data/Data';
+import axios from './axios-cards';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clickedCardId: null
+      clickedCardId: null,
+      Data: []
     }
+
     this.handleClick = this.handleClick.bind(this);
     this.handleReturnClick = this.handleReturnClick.bind(this);
   }
 
-  handleClick(id) {
+  componentDidMount() {
+
+      axios.get('/cards')
+          .then(res => {
+           const fetchedCards = [];
+              for (let key in res.data) {
+                  fetchedCards.push({
+                      ...res.data[key],
+                      id: key
+                  });
+              }
+              this.setState({loading: false, Data: fetchedCards});
+              })
+              .catch(err => {
+                  this.setState({loading: false});
+          })
+
+ }
+
+  handleClick = (id) => {
     this.setState({clickedCardId: id});
   }
 
-  handleReturnClick() {
+  handleReturnClick = () => {
     this.setState({clickedCardId: null});
   }
 
-  render() {
-    let selectedCards = Data.filter(Card => Card.id === this.state.clickedCardId)
+  render = () => {
+
+    let selectedCards = this.state.Data.filter(Card => Card.id === this.state.clickedCardId)
     return (
       <React.Fragment>
         {this.state.clickedCardId === null ?
-          <Cards handleClick={this.handleClick} data={Data} /> :
+          <Cards handleClick={this.handleClick} data={this.state.Data} /> :
           <Card
             Card={selectedCards[0]} handleReturnClick={this.handleReturnClick}
           />
@@ -38,5 +61,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
