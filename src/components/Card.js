daab2b01b720817bd7ebe45d9d188b0c6f8a7da5
Mona Temchin <monatemchin@gmail.com>
2018-03-08 14:24:56 -0500

    Refactored Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 import './Cardline.css';
 
-const Card = props => (
-    <div className="card" onClick={props.clicked}>
-            <h1 class="card-title">Esoteric Interpretation of {props.cardname}</h1>
-          <img className="card-img-top img-fluid" src={props.card_image_url} height="300" width="150" alt="Tarot card" />
-            <div class="card-body">
-            <p class="card-text">{props.cardinfo}</p>
-            </div>
-    </div>
-);
+
+const Card = ({card, handleReturnClick}) => {
+  return (
+    <React.Fragment>
+      <div className="card">
+              <h1 class="card-title">Esoteric Interpretation of {card.cardname}</h1>
+            <img className="card-img-top img-fluid" src={card.card_image_url} height="300" width="150" alt="Tarot card" />
+              <div class="card-body">
+              <p class="card-text">{card.cardinfo}</p>
+              </div>
+      </div>
+
+    </React.Fragment>
+  );
+}
 
 export default Card;
